3ce4501acb84c4b6e5f8803a89503663418bf72d
Jenni <jennioliver30@gmail.com>
2024-09-22 20:49:46 -0300

    adicionando js e css

--- scketch.js
+++ scketch.js
@@ -1,10 +1,14 @@
 function setup() {
     createCanvas(600, 600);
+    background("black")
+
   }
   
   function draw() {
     stroke(blue);
     fill("red");
-    if(mousePresed);
+    if(mouseIsPressed){
     rect(mouseX, mouseY, 20, 35);
   }
+
+}
